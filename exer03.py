d15a77f855244bca248685459b499c400ffac7a7
oldavidson <olidavidson.str@gmail.com>
2023-09-09 14:55:19 -0400

    Atualizando exercicios

---
+++ exer03.py
@@ -0,0 +1,4 @@
+num1 = int(input('Informe o primeiro número: '))
+num2 = int(input('Informe o segundo número: '))
+num3 = int(input('Informe o terceiro número: '))
+print('A soma dos números é: ', num1 + num2 + num3)
---
+++ exer04.py
@@ -0,0 +1,2 @@
+num1 = float(input('Informe um número: '))
+print ('O quadrado do número informado é: ', num1 ** 2)
