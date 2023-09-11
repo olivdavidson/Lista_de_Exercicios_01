4ffe7fa0a002f515f6aca9e6cf87567af79a1e93
Campos Conect <campos.conect@salonpas.local>
2023-09-11 15:21:34 -0400

    Atualização da lista de exercícios

---
+++ exer12.py
@@ -0,0 +1,3 @@
+dism = float(input('Informe a distância em milhas: '))
+disk = 1.61 * dism
+print('A distância em milhas convertida para Kilômetros é %.2f'%disk,'Km')
---
+++ exer13.py
@@ -0,0 +1,3 @@
+disk = float(input('Informe a distância em Kilômetros: '))
+dism = disk/1.61
+print('A distância em kilômetros convertida para milhas é %.2f'%dism)
