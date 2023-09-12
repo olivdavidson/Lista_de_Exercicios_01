5379953b4b38d517d326103ffe67dceaa6313c41
oldavidson <olidavidson.str@gmail.com>
2023-09-11 22:00:20 -0400

    Atualização de Lista 11092023

---
+++ exer14.py
@@ -0,0 +1,3 @@
+angg = float(input('Informe o valor do ângulo em Graus: '))
+angr = angg*(3.14/180)
+print('O valor do ângulo informado em Radianos é: %.2f'%angr)
---
+++ exer15.py
@@ -0,0 +1,3 @@
+angr = float(input('Informe o valor do ângulo em Radianos: '))
+angg = angr*(180/3.14)
+print('O valor do ângulo informado em Graus é: %.2f'%angg)
